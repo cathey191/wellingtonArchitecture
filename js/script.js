c039c1b8535b76ba29f5a50bc646979b520f28a2
cathey191 <cathey191@hotmail.com>
2018-05-11 10:00:03 +1200

    Test images

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-  $('.your-class').slick({
-    setting-name: setting-value
-  });
+  $('.variable-width').slick({
+		dots: true,
+  	infinite: true,
+  	speed: 300,
+  	slidesToShow: 1,
+  	centerMode: true,
+  	variableWidth: true
+	});
 });
